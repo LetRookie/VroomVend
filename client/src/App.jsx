a988e645b1fe204686afa20c88d0e1de7e2f56b6
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-11-15 22:09:44 +0200

    Added catalog page and styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,20 @@ import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 import Footer from "./components/footer/Footer"
 import FeaturedCars from "./components/featured-cars/FeaturedCars"
+import Catalog from "./components/catalog-page/Catalog"
 
 function App() {
 
   return (
     <>
-      <Routes>
+    <Header />
+      {/* <Routes>
         <Route path="/" element={<Home />} />
-      </Routes>
+      </Routes> */}
+      <Catalog />
       
       <Footer />
+
 
 
     </>
---
+++ client/src/components/catalog-page/Catalog.jsx
@@ -0,0 +1,78 @@
+export default function Catalog () {
+    return (
+        <div class="catalog">
+        <ul>
+            <li>
+                <img
+                    src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pixelstalk.net%2Fwp-content%2Fuploads%2F2016%2F08%2FFree-Cars-Full-HD-Images-1080p.jpg&f=1&nofb=1&ipt=b6e91853646ce73174926630a9ce8e45438c3c111be81acde124ca51db263118&ipo=images" />
+                <div class="description">
+                    <h3>Audi</h3>
+                    <span>52.000lv</span>
+                    <p>22.300km</p>
+                    <p>Sofia</p>
+                </div>
+            </li>
+            <li>
+                <img
+                    src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pixelstalk.net%2Fwp-content%2Fuploads%2F2016%2F08%2FFree-Cars-Full-HD-Images-1080p.jpg&f=1&nofb=1&ipt=b6e91853646ce73174926630a9ce8e45438c3c111be81acde124ca51db263118&ipo=images" />
+                <div class="description">
+                    <h3>Audi</h3>
+                    <p><b>52 000lv.</b></p>
+                    <p>22.300km</p>
+                    <p>Sofia</p>
+                </div>
+            </li>
+            <li>
+                <img
+                    src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pixelstalk.net%2Fwp-content%2Fuploads%2F2016%2F08%2FFree-Cars-Full-HD-Images-1080p.jpg&f=1&nofb=1&ipt=b6e91853646ce73174926630a9ce8e45438c3c111be81acde124ca51db263118&ipo=images" />
+                <div class="description">
+                    <h3>Audi</h3>
+                    <p>52 000lv.</p>
+                    <p>22.300km</p>
+                    <p>Sofia</p>
+                </div>
+            </li>
+            <li>
+                <img
+                    src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pixelstalk.net%2Fwp-content%2Fuploads%2F2016%2F08%2FFree-Cars-Full-HD-Images-1080p.jpg&f=1&nofb=1&ipt=b6e91853646ce73174926630a9ce8e45438c3c111be81acde124ca51db263118&ipo=images" />
+                <div class="description">
+                    <h3>Audi</h3>
+                    <p>52 000lv.</p>
+                    <p>22.300km</p>
+                    <p>Sofia</p>
+                </div>
+            </li>
+            <li>
+                <img
+                    src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pixelstalk.net%2Fwp-content%2Fuploads%2F2016%2F08%2FFree-Cars-Full-HD-Images-1080p.jpg&f=1&nofb=1&ipt=b6e91853646ce73174926630a9ce8e45438c3c111be81acde124ca51db263118&ipo=images" />
+                <div class="description">
+                    <h3>Audi</h3>
+                    <p>52 000lv.</p>
+                    <p>22.300km</p>
+                    <p>Sofia</p>
+                </div>
+            </li>
+            <li>
+                <img
+                    src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pixelstalk.net%2Fwp-content%2Fuploads%2F2016%2F08%2FFree-Cars-Full-HD-Images-1080p.jpg&f=1&nofb=1&ipt=b6e91853646ce73174926630a9ce8e45438c3c111be81acde124ca51db263118&ipo=images" />
+                <div class="description">
+                    <h3>Audi</h3>
+                    <p>52 000lv.</p>
+                    <p>22.300km</p>
+                    <p>Sofia</p>
+                </div>
+            </li>
+            <li>
+                <img
+                    src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pixelstalk.net%2Fwp-content%2Fuploads%2F2016%2F08%2FFree-Cars-Full-HD-Images-1080p.jpg&f=1&nofb=1&ipt=b6e91853646ce73174926630a9ce8e45438c3c111be81acde124ca51db263118&ipo=images" />
+                <div class="description">
+                    <h3>Audi</h3>
+                    <span></span>
+                    <span>22.300km</span>
+                    <span>Sofia</span>
+                </div>
+            </li>
+        </ul>
+    </div>
+    )
+}
