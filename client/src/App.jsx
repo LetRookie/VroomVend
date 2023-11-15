dab7975ba43ecf40ffce8c06aee4b0e8baf97160
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-11-15 12:10:30 +0200

    Added logo, Created Header and WelcomeHero components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,15 @@
-
+import Header from "./components/Header"
+import WelcomeHero from "./components/WelcomeHero"
 
 function App() {
 
   return (
     <>
       <section id="home" className="welcome-hero">
+        <Header />
+        
+        <WelcomeHero />
 
-        <div className="top-area">
-          <div className="header-area">
-            <nav className="navbar navbar-default bootsnav  navbar-sticky navbar-scrollspy" data-minus-value-desktop="70" data-minus-value-mobile="55" data-speed="1000">
-
-              <div className="container">
-
-                <div className="navbar-header">
-                  <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
-                    <i className="fa fa-bars"></i>
-                  </button>
-                  <a className="navbar-brand" href="index.html"><span>VroomVend</span></a>
-
-                </div>
-
-                <div className="collapse navbar-collapse menu-ui-design" id="navbar-menu">
-                  <ul className="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
-                    <li className=" scroll active"><a href="#home">home</a></li>
-                    <li className="scroll"><a href="#service">service</a></li>
-                    <li className="scroll"><a href="#featured-cars">featured cars</a></li>
-                    <li className="scroll"><a href="#new-cars">new cars</a></li>
-                    <li className="scroll"><a href="#brand">brands</a></li>
-                    <li className="scroll"><a href="#contact">contact</a></li>
-                  </ul>
-                </div>
-              </div>
-            </nav>
-
-          </div>
-          <div className="clearfix"></div>
-
-        </div>
-
-        <div className="container">
-          <div className="welcome-hero-txt">
-            <h2>get your desired car in resonable price</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore   magna aliqua.
-            </p>
-            <button className="welcome-btn" >contact us</button>
-          </div>
-        </div>
 
         <div className="container">
           <div className="row">
