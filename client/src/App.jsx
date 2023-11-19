4bd36a345602f3558252e5aea60fdfc3df1624ae
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-11-19 17:14:19 +0200

    Added footer and styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import './assets/styles/styles.css'
 import Home from "./components/home/Home"
 import LastAdded from "./components/last-added/LastAdded"
+import Footer from "./components/footer/Footer"
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
     <Home />
     <LastAdded />
+    <Footer/>
     </>
   )
 }
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+    return (
+        <>
+            <div className="footer-wrapper">
+                <div className="footer-list">
+                    <ul className="list-item">
+                        <li>
+                            <h6>About us</h6>
+                        </li>
+                        <li>
+                            <h6>About us</h6>
+                        </li>
+                        <li>
+                            <h6>About us</h6>
+                        </li>
+                    </ul>
+                </div>
+                <p>All rights reserver &copy; 2023</p>
+            </div>
+        </>
+    )
+}
