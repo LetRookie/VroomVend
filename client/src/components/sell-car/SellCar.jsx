f65b133b7fde6e3a6869b5d0c4561d9105db1dc1
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-11-20 11:10:51 +0200

    Added styles to sell-car

--- client/src/components/sell-car/SellCar.jsx
+++ client/src/components/sell-car/SellCar.jsx
@@ -3,18 +3,22 @@ export default function SellCar() {
         <section id="sell page" className="sell-car">
             <form className="sellForm">
                 <h2>Sell your car here:</h2>
-                <label htmlFor="brand-name">Brand:</label>
-                <input type="text" name="brand-name" id="brand-name" placeholder="Enter brand of car..."/>
-                <label htmlFor="model">Model:</label>
-                <input type="text" name="model" id="model" placeholder="Enter model of the brand..."/>
-                <label htmlFor="year">Brand:</label>
-                <input type="text" name="year" id="year" placeholder="Enter production year..."/>
-                <label htmlFor="km">Brand:</label>
-                <input type="text" name="km" id="km" placeholder="Enter km driven..."/>
-                <label htmlFor="color">Brand:</label>
-                <input type="text" name="color" id="color" placeholder="Enter color of car..."/>
+                <div className="form-wrapper">
+                    <label htmlFor="brand-name">Brand:</label>
+                    <input type="text" name="brand-name" id="brand-name" placeholder="   Enter brand of car..." />
+                    <label htmlFor="model">Model:</label>
+                    <input type="text" name="model" id="model" placeholder="   Enter model of the brand..." />
+                    <label htmlFor="year">Brand:</label>
+                    <input type="text" name="year" id="year" placeholder="   Enter production year..." />
+                    <label htmlFor="km">Brand:</label>
+                    <input type="text" name="km" id="km" placeholder="   Enter km driven..." />
+                    <label htmlFor="color">Brand:</label>
+                    <input type="text" name="color" id="color" placeholder="   Enter color of car..." />
+                    <label htmlFor="car-image">Brand:</label>
+                    <input type="text" name="car-image" id="car-image" placeholder="   Image url..." />
 
-                <input className="btn submit" type="submit" value="Sell Car" />
+                    <input className="btn submit" type="submit" value="Sell Car" />
+                </div>
             </form>
 
         </section>
