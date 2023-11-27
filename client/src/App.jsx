9eb389c1cf58ecd801a4ce85e02f4e850f158690
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-11-27 10:24:50 +0200

    authService modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
-import { Routes, Route } from "react-router-dom"
-import { useState } from "react"
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-import AuthContext from "./contexts/authContext"
-import './assets/styles/styles.css'
+import AuthContext from "./contexts/authContext";
+import * as authService from "./services/authService";
+import Path from "./lib/paths";
+import './assets/styles/styles.css';
 
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
@@ -15,12 +17,24 @@ import Details from "./components/details/Details"
 
 
 function App() {
+    const navigate = useNavigate();
 
     const [auth, setAuth] = useState({});
 
-    const loginSubmitHandler = (values) => {
+    // {"email":"peter@abv.bg",
+    // "username":"Peter",
+    // "_id":"35c62d76-8152-4626-8712-eeb96381bea8",
+    // "accessToken":"4b0f85a9bf7b55d32c61de75b33cfeb4f978982c86a5ccea726bc1289e1095cd"}
 
-        console.log(values);
+    const loginSubmitHandler = async (values) => {
+
+        // NEED TRY CATCH BLOCK (ERROR HANDLING) FOR UNREGISTERED USERS(GUESTS)-NOTIFICATION
+
+        const result = await authService.login(values.email, values.password);
+
+        setAuth(result);
+
+        navigate(Path.Home);
 
     }
 
@@ -29,7 +43,7 @@ function App() {
             <>
                 <Header />
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path={Path.Home} element={<Home />} />
                     <Route path="/search" element={<Search />} />
                     <Route path="/sell" element={<SellCar />} />
                     <Route path="/login" element={<Login loginSubmitHandler={loginSubmitHandler} />} />
--- client/src/contexts/authContext.js
+++ client/src/contexts/authContext.js
@@ -2,5 +2,7 @@ import { createContext } from "react";
 
 const AuthContext = createContext();
 
+AuthContext.displayName= 'Authorization.Context';
+
 export default AuthContext;
 
